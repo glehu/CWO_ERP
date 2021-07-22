9c29252f0adc01bd1ad846b6fb2ee7197d408a9a
duffy <duffystar1@gmail.com>
2021-07-22 17:00:40 +0200

    Added Industrial + Sorted all genres

--- src/main/kotlin/modules/m1/M1Genres.kt
+++ src/main/kotlin/modules/m1/M1Genres.kt
@@ -3,21 +3,22 @@ package modules.m1
 fun getGenreList(): ArrayList<String>
 {
     return arrayListOf(
-        "Indie",
-        "Indie Rock",
-        "Indie Alternative",
-        "Punk",
-        "Punk Rock",
-        "Gothic Rock",
         "Blues",
-        "RnB",
-        "Hip Hop",
-        "Trap",
-        "Techno",
         "Dubstep",
-        "Trance",
-        "Psytrance",
         "Glitchhop",
-        "Hyperpop"
+        "Gothic Rock",
+        "Hip Hop",
+        "Hyperpop",
+        "Indie Alternative",
+        "Indie Rock",
+        "Indie",
+        "Industrial",
+        "Psytrance",
+        "Punk Rock",
+        "Punk",
+        "RnB",
+        "Techno",
+        "Trance",
+        "Trap"
     )
 }
