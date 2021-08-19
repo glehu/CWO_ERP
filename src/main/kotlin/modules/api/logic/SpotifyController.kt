54424f557d3446b8102eed9a37cd2a718f83f19c
duffy <duffystar1@gmail.com>
2021-08-19 23:06:17 +0200

    Added SpotifyController.kt

---
+++ src/main/kotlin/modules/api/logic/SpotifyController.kt
@@ -0,0 +1,49 @@
+package modules.api.logic
+
+import interfaces.IAPI
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import modules.api.json.SpotifyUserProfileJson
+import modules.mx.getModulePath
+import tornadofx.Controller
+import java.io.File
+
+class SpotifyController : IAPI, Controller()
+{
+    override val apiName = "spotify"
+    override val auth = SpotifyAUTH()
+
+    fun getUserData(): SpotifyUserProfileJson
+    {
+        val userDataFile = getUserDataFile()
+        val fileContent = userDataFile.readText()
+        val userData: SpotifyUserProfileJson
+        if (fileContent.isEmpty())
+        {
+            if (auth.getAccessAndRefreshTokenFromDisk().access_token != "?")
+            {
+                userData = SpotifyAPI().getAccountData()
+                saveUserData(userData)
+            } else userData = SpotifyUserProfileJson()
+        } else
+        {
+            userData = Json.decodeFromString(fileContent)
+        }
+        return userData
+    }
+
+    private fun getUserDataFile(): File
+    {
+        val filePath = File("${getModulePath("MX")}\\api\\$apiName")
+        if (!filePath.isDirectory) filePath.mkdirs()
+        val userDataFile = File("$filePath\\${apiName}_user.json")
+        if (!userDataFile.isFile) userDataFile.createNewFile()
+        return userDataFile
+    }
+
+    private fun saveUserData(userData: SpotifyUserProfileJson)
+    {
+        getUserDataFile().writeText(Json.encodeToString(userData))
+    }
+}
