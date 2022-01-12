5c190c0b74191fbda47fb763c58f4ef86c653d59
glehu <duffystar1@gmail.com>
2022-01-13 00:18:10 +0100

    JCenter apparently obsolete -> removed and replaced by GradlePluginPortal

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
+    gradlePluginPortal()
     maven {
         name = "komputing/KHash GitHub Packages"
         url = uri("https://maven.pkg.github.com/komputing/KHash")
